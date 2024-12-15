2faa40001301c0044614bcdf57be3ed3f4e69274
Raphael Pontes <raphaelkennedy@hotmail.com>
2024-12-14 22:51:00 -0300

    remove change notify of view model

--- lib/ui/home/view_models/home_view_model.dart
+++ lib/ui/home/view_models/home_view_model.dart
@@ -1,6 +1,3 @@
-// Flutter imports:
-import 'package:flutter/material.dart';
-
 // Package imports:
 import 'package:logging/logging.dart';
 
@@ -13,7 +10,7 @@ import 'package:flutter_oficial_architecture/domain/use_cases/book/list_books_us
 import 'package:flutter_oficial_architecture/utils/command.dart';
 import 'package:flutter_oficial_architecture/utils/result.dart';
 
-class HomeViewModel extends ChangeNotifier {
+class HomeViewModel {
   HomeViewModel(
     this._listBooksUsecase,
     this._addBookUsecase,
@@ -42,68 +39,52 @@ class HomeViewModel extends ChangeNotifier {
   List<Book> get books => _books;
 
   Future<Result> _load() async {
-    try {
-      await Future.delayed(const Duration(seconds: 1));
-      final result = await _listBooksUsecase.execute();
-      switch (result) {
-        case Ok<List<Book>>():
-          _books = result.value;
-          _log.fine('Loaded bookings');
-        case Error<List<Book>>():
-          _log.warning('Failed to load bookings', result);
-      }
-
-      return result;
-    } finally {
-      notifyListeners();
+    await Future.delayed(const Duration(seconds: 1));
+    final result = await _listBooksUsecase.execute();
+    switch (result) {
+      case Ok<List<Book>>():
+        _books = result.value;
+        _log.fine('Loaded bookings');
+      case Error<List<Book>>():
+        _log.warning('Failed to load bookings', result);
     }
+
+    return result;
   }
 
   Future<Result<void>> _add(Book book) async {
-    try {
-      final result = await _addBookUsecase.execute(book);
-      switch (result) {
-        case Ok<void>():
-          _log.fine('Added booking');
-        case Error<void>():
-          _log.warning('Failed to add booking', result);
-      }
-
-      return result;
-    } finally {
-      notifyListeners();
+    final result = await _addBookUsecase.execute(book);
+    switch (result) {
+      case Ok<void>():
+        _log.fine('Added booking');
+      case Error<void>():
+        _log.warning('Failed to add booking', result);
     }
+
+    return result;
   }
 
   Future<Result<void>> _edit(Book book) async {
-    try {
-      final result = await _editBookUsecase.execute(book);
-      switch (result) {
-        case Ok<void>():
-          _log.fine('Edited booking');
-        case Error<void>():
-          _log.warning('Failed to edit booking', result);
-      }
-
-      return result;
-    } finally {
-      notifyListeners();
+    final result = await _editBookUsecase.execute(book);
+    switch (result) {
+      case Ok<void>():
+        _log.fine('Edited booking');
+      case Error<void>():
+        _log.warning('Failed to edit booking', result);
     }
+
+    return result;
   }
 
   Future<Result<void>> _delete(String id) async {
-    try {
-      final result = await _deleteBookUsecase.execute(id);
-      switch (result) {
-        case Ok<void>():
-          _log.fine('Deleted booking');
-        case Error<void>():
-          _log.warning('Failed to delete booking', result);
-      }
-
-      return result;
-    } finally {
-      notifyListeners();
+    final result = await _deleteBookUsecase.execute(id);
+    switch (result) {
+      case Ok<void>():
+        _log.fine('Deleted booking');
+      case Error<void>():
+        _log.warning('Failed to delete booking', result);
     }
+
+    return result;
   }
 }
